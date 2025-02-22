2c84a2747f6d848970e0210d6303749c620973e8
centerbird <centerbirdx1@gmail.com>
2025-02-22 17:25:16 +0100

    feat : added working button

---
+++ src/HUD/hud.gd
@@ -0,0 +1,10 @@
+class_name HUD extends Node2D
+## Defines the bahaviour of the HUD.
+##
+## @experimental : this class is not yet finished
+
+
+
+# Decrese all healthbars by one TODO
+func _on_timer_timeout() -> void:
+	pass
