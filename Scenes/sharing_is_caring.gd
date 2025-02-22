6582d66cd3745ecae265c79c94e9a68a20c78fea
centerbird <centerbirdx1@gmail.com>
2025-02-22 21:46:57 +0100

    ... : I don't know at this point and I'm too afraid to try to understand

---
+++ Scenes/sharing_is_caring.gd
@@ -0,0 +1,7 @@
+class_name SharingIsCaring extends Node2D
+## TODO
+##
+## @experimental : not finished
+
+## Colors associated with [Village]s in order of appearance.
+const VillageColors : Array[Color] = [Color.CYAN, Color.MAGENTA, Color.GOLDENROD, Color.LAWN_GREEN, Color.TURQUOISE]
--- src/HUD/health/health_bar.gd
+++ src/HUD/health/health_bar.gd
@@ -6,8 +6,11 @@ class_name HealthBar extends Node2D
 ## Amount of health a village has.
 @export var health : int = 7
 
-## The width until which the [HealthBar] should stretch (in pixels).
+## The width until which the units of health on the [HealthBar] should stretch (in pixels/scale) after village symbol.
 @export var width : int = 900
+
+## The distance by which health units are separated from one another (in pixels/scale).
+@export var unit_separation_distance : int = 5
 
 ## Unit representing a single health point.
 @export var unit := preload("res://Scenes/HUD/health/health_unit.tscn")
@@ -17,6 +20,13 @@ signal game_over
 
 # current amount of health
 var _current_health := 0
+
+func _ready():
+	_current_health = health
+
+# Spawns the health bar TODO
+func _initiate_health_bar():
+	pass
 
 ## Increases health by one, up to its maximum. TODO
 func gain_health():
